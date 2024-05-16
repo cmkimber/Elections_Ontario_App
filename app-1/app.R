0cd960b37ca13386dce46e1d0ace1f946568fe6e
cmkimber <cmkimber@gmail.com>
2024-05-16 18:16:44 -0400

    clean app code line breaks

--- app-1/app.R
+++ app-1/app.R
@@ -29,7 +29,12 @@ ui <- fluidPage(
   titlePanel("Election Results Map"),
   helpText("To see the results of the 2022 General Election in a given riding, select the riding from the drop-down menu or click on the riding in the map. Note that you may search for a specific riding in the drop-down menu by typing its name or ID number."),
   # note selectize is used here with multiple selections enabled and a max number of selections of 1 to facilitate having the app initialize with no district selected
-  selectizeInput(inputId = "district", label = "Electoral District", choices = sort(choices), multiple = TRUE, selected = NULL, options = list(maxItems = 1)),
+  selectizeInput(inputId = "district", 
+                 label = "Electoral District", 
+                 choices = sort(choices), 
+                 multiple = TRUE, 
+                 selected = NULL, 
+                 options = list(maxItems = 1)),
   dataTableOutput("district_results"),
   leafletOutput("mymap")
 )
@@ -47,8 +52,15 @@ server <- function(input, output, session){
     updateSelectInput(session, inputId = "district", selected = district_input_filter())
     proxy %>% clearGroup(group = "highlighted_district")
     #proxy %>% addPolylines(stroke = TRUE, weight = 4, color = "yellow", data = filter(electoral_winners_WGS84, ElectoralDistrictNameEnglish == district_input_filter()), group = "highlighted_district")
-    proxy %>% addPolygons(stroke = TRUE, weight = 4, color = "yellow", fillColor = "white", fillOpacity = 0.5, data = filter(electoral_winners_WGS84, ElectoralDistrictNameEnglish == district_input_filter()), group = "highlighted_district")
-    bbox <- filter(electoral_winners_WGS84, ElectoralDistrictNameEnglish == district_input_filter()) %>% get_bounding_box()
+    proxy %>% addPolygons(stroke = TRUE,
+                          weight = 4,
+                          color = "yellow",
+                          fillColor = "white",
+                          fillOpacity = 0.5,
+                          data = filter(electoral_winners_WGS84, ElectoralDistrictNameEnglish == district_input_filter()),
+                          group = "highlighted_district")
+    bbox <- filter(electoral_winners_WGS84, ElectoralDistrictNameEnglish == district_input_filter()) %>%
+      get_bounding_box()
     proxy %>% flyToBounds(lng1 = bbox[["xmin"]], lat1 = bbox[["ymin"]], lng2 = bbox[["xmax"]], lat2 = bbox[["ymax"]])
   })
   
@@ -63,8 +75,19 @@ server <- function(input, output, session){
   # table output uses req() to require a value from an input so it doesn't render on launch
   output$district_results <- DT::renderDataTable({
     req(!is.null(district_input_filter()))
-    df_out <- electoral_results_2022 %>% filter(ElectoralDistrictNameEnglish == district_input_filter()) %>% select(c(PoliticalInterestCode, NameOfCandidates, TotalValidBallotsCast, PercentOfTotalValidBallotsCast))
-    datatable(df_out, options = list(dom = "t", searching = FALSE, order = list(list(3, "desc"))), colnames = c("Party", "Candidate", "Votes", "%")) %>% formatPercentage("PercentOfTotalValidBallotsCast", digits = 2) %>% formatStyle("PoliticalInterestCode", backgroundColor = styleEqual(levels = levels(electoral_winners_WGS84$PoliticalInterestCode), values = c("#A6D43D", "#6E6E6E", "#E06666", "#FFA257", "#498BE7"), default = NULL))
+    df_out <- electoral_results_2022 %>%
+      filter(ElectoralDistrictNameEnglish == district_input_filter()) %>%
+      select(c(PoliticalInterestCode, NameOfCandidates, TotalValidBallotsCast, PercentOfTotalValidBallotsCast))
+    datatable(df_out,
+              options = list(dom = "t",
+                             searching = FALSE,
+                             order = list(list(3, "desc"))),
+              colnames = c("Party", "Candidate", "Votes", "%")) %>%
+      formatPercentage("PercentOfTotalValidBallotsCast", digits = 2) %>%
+      formatStyle("PoliticalInterestCode",
+                  backgroundColor = styleEqual(levels = levels(electoral_winners_WGS84$PoliticalInterestCode),
+                                               values = c("#A6D43D", "#6E6E6E", "#E06666", "#FFA257", "#498BE7"),
+                                               default = NULL))
   })
   
   #define the popup text  
@@ -79,7 +102,16 @@ server <- function(input, output, session){
   )
   
   output$mymap <- renderLeaflet({
-    leaflet(electoral_winners_WGS84) %>% addPolygons(layerId = ~electoral_winners_WGS84$ElectoralDistrictNameEnglish, highlightOptions = highlightOptions(color = "white", weight = 2, fillOpacity = 0.2), color = "black", weight = 1, fillColor = ~factpal(PoliticalInterestCode), fillOpacity = 1, popup = popup_text)
+    leaflet(electoral_winners_WGS84) %>%
+      addPolygons(layerId = ~electoral_winners_WGS84$ElectoralDistrictNameEnglish,
+                  highlightOptions = highlightOptions(color = "white",
+                                                      weight = 2,
+                                                      fillOpacity = 0.2),
+                  color = "black",
+                  weight = 1,
+                  fillColor = ~factpal(PoliticalInterestCode),
+                  fillOpacity = 1,
+                  popup = popup_text)
   })
 }
 
