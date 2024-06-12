3f23ea70ce58d7d94b43470a6f7591bd14757c14
cmkimber <cmkimber@gmail.com>
2024-06-12 15:00:29 -0400

    add pane 3 to main app

--- elections_ontario_app/app.R
+++ elections_ontario_app/app.R
@@ -8,6 +8,7 @@ library(DT)
 library(glue)
 library(ggiraph)
 library(ggrepel)
+library(plotly)
 
 ### LOAD PANEL 1 DATA, OBJECTS & FUNCTIONS ----
 
@@ -48,6 +49,22 @@ get_popup_text <- function(polygon_row){
 
 election_and_party <- readRDS("./data/election_and_party.rds")
 
+### LOAD PANEL 3 DATA AND OBJECTS ----
+
+election_seat_ballot <- readRDS("./data/election_seat_ballot.rds")
+
+# set a colour palette for each party that is robust to changes in which party is selected to display in the plot
+colour_table <- election_seat_ballot %>%
+  ungroup() %>%
+  select(c(Party, Party.Col)) %>%
+  distinct(Party, .keep_all = TRUE)
+pane_3_palette <- setNames(colour_table$Party.Col, colour_table$Party)
+
+response_options <- c("Seats" = "Seats.Won",
+                      "Ballots" = "Votes.Cast",
+                      "% Seats" = "Pct.Seats",
+                      "% Ballots" = "Pct.Votes")
+
 ### BUILD UI ----
 
 ui <- fluidPage(
@@ -82,6 +99,32 @@ ui <- fluidPage(
         mainPanel = mainPanel(
           girafeOutput(outputId = "donut_plot", height = 500)
         )
+      )
+    ),
+    tabPanel("Seats/Ballots Won by Parties",
+      titlePanel("Seats/Ballots Won by Parties"),
+      helpText("This visualization explores the success of parties over time. Use the dropdown menu to choose which parties you wish to display. Use the controls above the graph to switch between vote totals and legislature seat totals per party, either in absolute values or as percentages of total. Control the time period using the selector bar below the graph."),
+      fluidRow(
+       column(8,
+              selectizeInput(inputId = "party",
+                             label = "Parties",
+                             choices = unique(election_seat_ballot$Party),
+                             selected = c("Ontario Liberal Party", "Green Party of Ontario", "Progressive Conservative Party of Ontario", "New Democratic Party of Ontario", "Family Coalition Party"),
+                             multiple = TRUE)
+       ),
+       column(4,
+              checkboxInput(inputId = "percentage", 
+                            label = "Show percentage",
+                            FALSE),
+              radioButtons(inputId = "seats_ballots",
+                           label = "",
+                           choices = c("Seats" = "Seats.Won",
+                                       "Ballots" = "Votes.Cast"))
+       )
+      ),
+      fluidRow(
+       plotlyOutput("pane_3_plot"),
+       DTOutput("pane_3_table")
       )
     )
   )
@@ -256,6 +299,80 @@ server <- function(input, output, session){
             axis.text = element_blank())
     
     girafe(ggobj = panel_2_plot)
+  })
+  
+### PANE 3 SERVER CONTENTS ----
+  
+  parties_to_display <- reactive({
+    election_seat_ballot %>% filter(Party %in% input$party)
+  })
+  
+  # make the hidden choices underlying the radio buttons responsive to whether total or percent values are selected
+  observeEvent(input$percentage, {
+    if (input$percentage == FALSE){
+      updateRadioButtons(session,
+                         inputId = "seats_ballots",
+                         choices = c("Seats" = "Seats.Won",
+                                     "Ballots" = "Votes.Cast"))
+    }
+    else if (input$percentage == TRUE){
+      updateRadioButtons(session,
+                         inputId = "seats_ballots",
+                         choices = c("Seats" = "Pct.Seats",
+                                     "Ballots" = "Pct.Votes"))
+    }
+  })
+  
+  output$pane_3_plot <- renderPlotly({
+    # response <- switch(input$seats_ballots,
+    #                    "Seats.Won" = parties_to_display()$Seats.Won,
+    #                    "Votes.Cast" = parties_to_display()$Votes.Cast,
+    #                    Seats.Won)
+    
+    pane_3_plot <- ggplot(data = parties_to_display(),
+                          aes(x = Year,
+                              y = .data[[input$seats_ballots]],
+                              group = Party,
+                              text = paste(Party, Year, .data[[input$seats_ballots]], sep = "\n"))) +
+      geom_line(aes(color = Party)) +
+      labs(x = "Year",
+           y = names(response_options[which(response_options == input$seats_ballots)])) +
+      scale_color_manual(values = pane_3_palette) +
+      theme_light() +
+      theme(panel.grid.major.x = element_blank(),
+            panel.grid.minor.x = element_blank(),
+            panel.grid.minor.y = element_blank(),
+            legend.position = "none")
+    
+    ggplotly(pane_3_plot, dynamicTicks = TRUE, tooltip = "text") %>%
+      rangeslider() %>%
+      layout(hovermode = "x") %>%
+      style(mode = "markers+lines")
+  })
+  
+  # Store information on which year is selected in plot in a reactive value and observe to maintain the saved year until a new year is hovered over. This avoids the year being set to NULL when moving between points and blanking the DataTable.
+  chosen_year <- reactiveVal()
+  observe({
+    req(!is.null(chosen_year()))
+    hover_data <- event_data(event = "plotly_hover")$x[1]
+    if(!is.null(hover_data))
+      chosen_year(hover_data)
+  })
+  
+  output$pane_3_table <- DT::renderDataTable({
+    req(!is.null(chosen_year()))
+    df_pane_3_out <- parties_to_display() %>%
+      select(c(Party, Year, input$seats_ballots)) %>%
+      filter(Year == chosen_year())
+    datatable(df_pane_3_out,
+              rownames = FALSE,
+              options = list(dom = "t",
+                             searching = FALSE,
+                             order = list(list(2, "desc"))),
+              colnames = c("Party", "Year", names(response_options[which(response_options == input$seats_ballots)]))) %>%
+      formatStyle("Party",
+                  backgroundColor = styleEqual(levels = c("Green Party of Ontario", "Ontario Liberal Party", "New Democratic Party of Ontario", "Progressive Conservative Party of Ontario"),
+                                               values = c("#A6D43D", "#E06666", "#FFA257", "#498BE7")))
   })
   
 }
