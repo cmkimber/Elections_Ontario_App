8ed92a15c5f08aba82177bd96f2fce688e33273d
cmkimber <cmkimber@gmail.com>
2024-06-13 19:54:25 -0400

    update formatting for pane 5 graph

--- elections_ontario_app/pane_5_app_temp.R
+++ elections_ontario_app/pane_5_app_temp.R
@@ -22,27 +22,28 @@ server <- function(input, output, session){
   
   output$pane_5_plot <- renderPlotly({
     pane_5_plot <- plot_ly(election_results,
-                           x = ~Year,
-                           y = ~Voter.Turnout,
-                           text = ~Pct.Turnout,
-                           name = "Voter Turnout",
-                           type = "scatter",
-                           mode = "lines+markers", 
-                           fill = 'tozeroy',
-                           line = list(width = 0.75,
-                                       color = "#0000006b"),
-                           #marker = list(color = "#0000006b"),
-                           fillcolor = "#ffcf00",
-                           texttemplate = "%{text:.0%}",
-                           textposition = "outside") %>%
-      add_trace(x = ~Year,
-                y = ~Registered.Voters,
+                           x = ~Year) %>%
+      add_trace(y = ~Voter.Turnout,
+                text = ~Pct.Turnout,
+                name = "Voter Turnout",
+                type = "scatter",
+                mode = "lines+markers",
+                fill = 'tozeroy',
+                line = list(width = 0.75,
+                           color = "#0000006b"),
+                marker = list(color = "#0000006b"),
+                fillcolor = "#ffcf00",
+                hovertemplate = paste("%{y:,d} (%{text:.2%})"),
+                texttemplate = "%{text:.0%}",
+                textposition = "outside") %>%
+      add_trace(y = ~Registered.Voters,
                 name = "Total Voters",
                 mode = "lines",
                 fill = "none",
                 line = list(width = 2,
                             color = "#0000006b",
-                            dash = "dash")) %>%
+                            dash = "dash"),
+                hovertemplate = paste("%{y:,d}")) %>%
       rangeslider(start = 1867, end = 1940) %>%
       layout(xaxis = list(title = "Years",
                           ticks = "outside"),
