abc09df85d19ca55a6f0e3d61c79807590bd56d8
cmkimber <cmkimber@gmail.com>
2024-06-13 19:56:05 -0400

    add help text to pane 5 plot

--- elections_ontario_app/pane_5_app_temp.R
+++ elections_ontario_app/pane_5_app_temp.R
@@ -13,8 +13,8 @@ library(plotly)
 election_results <- readRDS("./data/election_turnout.rds")
 
 ui <- fluidPage(
-  titlePanel("holder"),
-  helpText("holder"),
+  titlePanel("Historical Voter Turnout"),
+  helpText("This graph shows the total number of registered voters and actual voter turnout for each election from 1867 to 2022. To change the time period shown, use the selector below the plot."),
   plotlyOutput("pane_5_plot")
 )
 
@@ -23,19 +23,6 @@ server <- function(input, output, session){
   output$pane_5_plot <- renderPlotly({
     pane_5_plot <- plot_ly(election_results,
                            x = ~Year) %>%
-      add_trace(y = ~Voter.Turnout,
-                text = ~Pct.Turnout,
-                name = "Voter Turnout",
-                type = "scatter",
-                mode = "lines+markers",
-                fill = 'tozeroy',
-                line = list(width = 0.75,
-                           color = "#0000006b"),
-                marker = list(color = "#0000006b"),
-                fillcolor = "#ffcf00",
-                hovertemplate = paste("%{y:,d} (%{text:.2%})"),
-                texttemplate = "%{text:.0%}",
-                textposition = "outside") %>%
       add_trace(y = ~Registered.Voters,
                 name = "Total Voters",
                 mode = "lines",
@@ -44,6 +31,19 @@ server <- function(input, output, session){
                             color = "#0000006b",
                             dash = "dash"),
                 hovertemplate = paste("%{y:,d}")) %>%
+      add_trace(y = ~Voter.Turnout,
+                text = ~Pct.Turnout,
+                name = "Voter Turnout",
+                mode = "lines+markers+text",
+                fill = 'tozeroy',
+                line = list(width = 0.75,
+                           color = "#0000006b"),
+                marker = list(color = "#0000006b"),
+                fillcolor = "#ffcf00",
+                textfont = list(size = 8),
+                hovertemplate = paste("%{y:,d} (%{text:.2%})"),
+                texttemplate = "%{text:.0%}",
+                textposition = "top") %>%
       rangeslider(start = 1867, end = 1940) %>%
       layout(xaxis = list(title = "Years",
                           ticks = "outside"),
