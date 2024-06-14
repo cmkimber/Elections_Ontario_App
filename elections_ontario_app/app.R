8bdd10c0be8e2a352ee39bd700d05aa4865c8757
cmkimber <cmkimber@gmail.com>
2024-06-14 09:24:08 -0400

    add pane 5 to main app

--- elections_ontario_app/app.R
+++ elections_ontario_app/app.R
@@ -1,4 +1,4 @@
-#setwd("~/Documents/Elections_Ontario_App/elections_ontario_app")
+setwd("~/Documents/Elections_Ontario_App/elections_ontario_app")
 
 library(tidyverse)
 library(sf)
@@ -10,7 +10,7 @@ library(ggiraph)
 library(ggrepel)
 library(plotly)
 
-### LOAD PANEL 1 DATA, OBJECTS & FUNCTIONS ----
+### LOAD PANE 1 DATA, OBJECTS & FUNCTIONS ----
 
 electoral_districts_WGS84 <- readRDS("./data/electoral_districts_WGS84.rds")
 electoral_results <- readRDS("./data/electoral_results.rds")
@@ -45,11 +45,11 @@ get_popup_text <- function(polygon_row){
   return(popup_text)
 }
 
-### LOAD PANEL 2 DATA ----
+### LOAD PANE 2 DATA ----
 
 election_and_party <- readRDS("./data/election_and_party.rds")
 
-### LOAD PANEL 3 DATA AND OBJECTS ----
+### LOAD PANE 3 DATA AND OBJECTS ----
 
 election_seat_ballot <- readRDS("./data/election_seat_ballot.rds")
 
@@ -65,9 +65,13 @@ response_options <- c("Seats" = "Seats.Won",
                       "% Seats" = "Pct.Seats",
                       "% Ballots" = "Pct.Votes")
 
-### LOAD PANEL 4 DATA ----
+### LOAD PANE 4 DATA ----
 
 election_district_turnout <- readRDS(file = "./data/election_district_turnout.rds")
+
+### LOAD PANE 5 DATA ----
+
+election_results <- readRDS("./data/election_turnout.rds")
 
 ### BUILD UI ----
 
@@ -139,7 +143,12 @@ ui <- fluidPage(
                       choices = sort(unique(election_district_turnout$Year), decreasing = TRUE),
                       selected = 2022),
        plotlyOutput("pane_4_plot")
-    )
+    ),
+    tabPanel("Historical Voter Turnout",
+             titlePanel("Historical Voter Turnout"),
+             helpText("This graph shows the total number of registered voters and actual voter turnout for each election from 1867 to 2022. To change the time period shown, use the selector below the plot."),
+             plotlyOutput("pane_5_plot")
+             )
   )
 )
 
@@ -420,6 +429,40 @@ server <- function(input, output, session){
                           nticks = 8,
                           tickformat = ",d"),
              barmode = "group",
+             hovermode = "x unified")
+    
+  })
+
+### PANE 5 SERVER CONTENTS ----
+  
+  output$pane_5_plot <- renderPlotly({
+    pane_5_plot <- plot_ly(election_results,
+                           x = ~Year) %>%
+      add_trace(y = ~Registered.Voters,
+                name = "Total Voters",
+                mode = "lines",
+                fill = "none",
+                line = list(width = 2,
+                            color = "#0000006b",
+                            dash = "dash"),
+                hovertemplate = paste("%{y:,d}")) %>%
+      add_trace(y = ~Voter.Turnout,
+                text = ~Pct.Turnout,
+                name = "Voter Turnout",
+                mode = "lines+markers+text",
+                fill = 'tozeroy',
+                line = list(width = 0.75,
+                            color = "#0000006b"),
+                marker = list(color = "#0000006b"),
+                fillcolor = "#ffcf00",
+                textfont = list(size = 8),
+                hovertemplate = paste("%{y:,d} (%{text:.2%})"),
+                texttemplate = "%{text:.0%}",
+                textposition = "top") %>%
+      rangeslider(start = 1867, end = 1940) %>%
+      layout(xaxis = list(title = "Years",
+                          ticks = "outside"),
+             yaxis = list(title = "Ballots"),
              hovermode = "x unified")
     
   })
